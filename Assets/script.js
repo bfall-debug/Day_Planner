51701947d71ef817f711b507530265d8e395e012
BFall-Debug <bfall92@gmail.com>
2019-10-23 15:24:09 -0400

    CSS fully utilized and aesthetics updated

--- Assets/script.js
+++ Assets/script.js
@@ -3,19 +3,20 @@ var time = moment()
 var WorkStarts = 9;
 var WorkEnds = 17;
 var TimeSlots = $(".container");
+$("#currentDay").text(time.format('dddd, MMMM Do'));
 
 for (i = WorkStarts; i < WorkEnds; i++) {
     var TimeBlock = moment(i, format)
     var NextTimeBlock = moment(i + 1, format)
 
     NewHour = $("<div>");
-    NewHour.addClass("row");
+    NewHour.addClass("row time-block");
 
     Hour = $("<div>");
     Hour.addClass("col-2 hour");
     Hour.text(TimeBlock.format(format));
 
-    TimeFrame = $("<input>");
+    TimeFrame = $("<textarea>");
     TimeFrame.addClass("col-8")
     if ( time.isBetween(TimeBlock,NextTimeBlock)) {
         TimeFrame.addClass("present");
@@ -23,13 +24,12 @@ for (i = WorkStarts; i < WorkEnds; i++) {
     else if (time.isAfter(TimeBlock)) {
         TimeFrame.addClass("past");
     }
-    
     else {
         TimeFrame.addClass("future");
     }
 
-    SaveButton = $("<div class='col-2 saveBtn'>");
-    SaveButton.addClass("col-2 saveBtn")
+    SaveButton = $("<button>");
+    SaveButton.addClass("col-2 saveBtn fas fa-save")
 
     NewHour.append(Hour, TimeFrame, SaveButton);
     TimeSlots.append(NewHour);
